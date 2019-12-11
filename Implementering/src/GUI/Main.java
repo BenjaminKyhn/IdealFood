a478ec4612b3162de19d18ee7dfed25e2a4c5bae
Patrick <patr6996@edu.easj.dk>
2019-12-11 15:09:30 +0100

    JavaFX sample.fxml fejl fikset

--- Implementering/src/GUI/Main.java
+++ Implementering/src/GUI/Main.java
@@ -1,28 +1,29 @@
 package GUI;
 
-import Logic.Ingredient;
+
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     private Scene startScene;
+    Stage stage = new Stage();
 
     @Override
 
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("GUI/Start.fxml"));
-        primaryStage.setTitle("IdealFood");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-
+        Stage root = FXMLLoader.load(getClass().getResource("start.fxml"));
+        root.show();
+//        primaryStage.setTitle("IdealFood");
+//        primaryStage.setScene(new Scene(root, 546, 822));
+//        primaryStage.show();
     }
 
 
@@ -30,15 +31,26 @@ public class Main extends Application {
         launch(args);
     }
 
-    public void popupChooseIngredients ()
+    public void popupChooseIngredients (javafx.event.ActionEvent actionEvent) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("chooseIngredients.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
 
-    /*@FXML
+        stage.setTitle("Choose Ingredients");
+        stage.setScene(new Scene(root1));
+        stage.show();
+
+
+
+    }
+
+
+
+    @FXML
     private TableColumn<TableSetterGetter, Logic.Ingredient> Ingredient;
 
     @FXML
-    private TableColumn<TableSetterGetter, CheckBox> select; */
+    private TableColumn<TableSetterGetter, CheckBox> select;
 
-    @FXML
-    private TableView()
+
 
 }
