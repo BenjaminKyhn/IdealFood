9131c474b9ab74296793b2f37280efe83b5915b4
Patrick <patr6996@edu.easj.dk>
2019-12-11 13:30:51 +0100

    JavaFX sample.fxml

--- Implementering/src/GUI/Main.java
+++ Implementering/src/GUI/Main.java
@@ -1,10 +1,14 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
+
+import javax.swing.text.TableView;
 
 public class Main extends Application {
 
@@ -20,4 +24,8 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    @FXML
+    private TableColumn<>
+
 }
