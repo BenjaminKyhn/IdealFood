4fc356f2a645bfb6ed982d51f2504569e2755378
Patrick <patr6996@edu.easj.dk>
2019-12-11 13:48:32 +0100

    JavaFX sample.fxml

--- Implementering/src/GUI/Main.java
+++ Implementering/src/GUI/Main.java
@@ -1,23 +1,28 @@
 package GUI;
 
+import Logic.Ingredient;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
-
-import javax.swing.text.TableView;
 
 public class Main extends Application {
 
+    private Scene startScene;
+
     @Override
+
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("GUI/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("GUI/Start.fxml"));
         primaryStage.setTitle("IdealFood");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
     }
 
 
@@ -25,7 +30,15 @@ public class Main extends Application {
         launch(args);
     }
 
+    public void popupChooseIngredients ()
+
+    /*@FXML
+    private TableColumn<TableSetterGetter, Logic.Ingredient> Ingredient;
+
     @FXML
-    private TableColumn<>
+    private TableColumn<TableSetterGetter, CheckBox> select; */
+
+    @FXML
+    private TableView()
 
 }
