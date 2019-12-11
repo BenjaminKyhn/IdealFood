9988d1f53c4abf8f712424d0bb77257416fbcce4
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-12-11 10:50:20 +0100

    Classes created

--- Implementering/src/Logic/Recipe.java
+++ Implementering/src/Logic/Recipe.java
@@ -1,7 +1,11 @@
 package Logic;
 
+import java.util.ArrayList;
+
 public class Recipe {
     int id;
     String name;
-
+    ArrayList<Ingredient> ingredients;
+    ArrayList<Double> portionSize;
+    int portionCount;
 }
