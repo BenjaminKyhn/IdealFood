3f4ee64dbbb67ec26812abc3516ba42bbfa2c2d3
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-12-11 14:20:09 +0100

    SQL database dump

---
+++ Implementering/src/Persistence/idealfood_ingredient_amount.sql
@@ -0,0 +1,119 @@
+create table `ingredient amount`
+(
+    id           int auto_increment
+        primary key,
+    RecipeID     int         null,
+    IngredientID int         null,
+    amount       double      null,
+    unit         varchar(10) null
+);
+
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (1, 1, 1, 1, 'pcs');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (2, 1, 2, 300, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (3, 1, 3, 40, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (4, 1, 7, 10, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (5, 1, 8, 10, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (6, 1, 13, 20, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (7, 1, 20, 10, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (8, 2, 1, 1, 'pcs');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (9, 2, 2, 300, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (10, 2, 10, 2, 'pcs');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (11, 2, 17, 50, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (12, 2, 4, 50, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (13, 2, 7, 40, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (14, 2, 8, 25, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (15, 3, 1, 1, 'pcs');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (16, 3, 2, 100, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (17, 3, 10, 1, 'pcs');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (18, 3, 15, 40, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (19, 3, 11, 15, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (20, 3, 13, 10, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (21, 3, 17, 15, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (22, 3, 7, 15, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (23, 4, 1, 1, 'pcs');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (24, 4, 9, 2, 'pcs');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (25, 4, 21, 0.5, 'tbsp');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (26, 4, 22, 1, 'pcs');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (27, 4, 23, 0.5, 'pck');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (28, 4, 24, 25, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (29, 4, 25, 0.5, 'tbsp');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (30, 4, 26, 1, 'tsp');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (31, 4, 15, 1.5, 'pcs');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (32, 4, 27, 0.5, 'tbsp');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (33, 4, 28, 0.5, 'tbsp');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (34, 4, 29, 1.5, 'pcs');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (35, 4, 17, 25, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (36, 4, 7, 10, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (37, 4, 3, 12.5, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (38, 5, 1, 1, 'pcs');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (39, 5, 7, 10, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (40, 5, 5, 10, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (41, 5, 6, 10, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (42, 5, 2, 125, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (43, 5, 18, 150, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (44, 5, 13, 5, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (45, 5, 30, 15, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (46, 5, 15, 10, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (47, 6, 1, 1, 'pcs');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (48, 6, 2, 100, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (49, 6, 7, 20, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (50, 6, 15, 10, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (51, 6, 12, 20, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (52, 6, 19, 10, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (53, 6, 22, 20, 'g');
+INSERT INTO idealfood.`ingredient amount` (id, RecipeID, IngredientID, amount, unit) VALUES (54, 6, 3, 20, 'g');
+create table ingredients
+(
+    id            int auto_increment
+        primary key,
+    name          varchar(50) null,
+    calories      double      null,
+    protein       double      null,
+    fat           double      null,
+    carbohydrates double      null
+);
+
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (1, 'burger bun', 202, 6, 12, 70);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (2, 'ground beef', 160, 20, 10, 0);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (3, 'cheddar cheese', 300, 20, 35, 5);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (4, 'mozzarella cheese', 300, 22, 30, 5);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (5, 'tomato', 18, 2, 0, 25);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (6, 'cucumber', 15, 0, 0, 17);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (7, 'lettuce', 10, 0, 0, 12);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (8, 'onion', 40, 5, 0, 40);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (9, 'mushroom', 22, 9, 0, 15);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (10, 'egg', 100, 10, 10, 0);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (11, 'asparges', 25, 6, 0, 24);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (12, 'guacamole', 300, 11, 40, 40);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (13, 'ketchup', 203, 4, 17, 68);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (14, 'hummus', 273, 8, 34, 29);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (15, 'red onion', 40, 5, 0, 40);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (16, 'paprika', 37, 4, 0, 31);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (17, 'mayonaise', 553, 5, 60, 40);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (18, 'sweet potatoe', 100, 7, 2, 50);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (19, 'broccoli', 35, 8, 0, 30);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (20, 'mustard', 251, 5, 30, 34);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (21, 'olive oil', 900, 0, 95, 0);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (22, 'red pepper', 37, 2, 0, 31);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (23, 'thyme', 0, 0, 0, 0);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (24, 'breadcrumb', 221, 5, 5, 67);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (25, 'sun dried tomato paste', 350, 5, 50, 25);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (26, 'smoked paprika', 0, 0, 0, 0);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (27, 'golden caster sugar', 400, 0, 0, 80);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (28, 'sherry vinegar', 70, 0, 0, 40);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (29, 'garlic cloves', 35, 4, 2, 25);
+INSERT INTO idealfood.ingredients (id, name, calories, protein, fat, carbohydrates) VALUES (30, 'carrot', 40, 2, 0, 40);
+create table recipes
+(
+    id    int auto_increment
+        primary key,
+    name  varchar(50)  null,
+    howto varchar(500) null
+);
+
+INSERT INTO idealfood.recipes (id, name, howto) VALUES (1, 'Beefaholic', '');
+INSERT INTO idealfood.recipes (id, name, howto) VALUES (2, 'Eggylicious', null);
+INSERT INTO idealfood.recipes (id, name, howto) VALUES (3, 'Hella Home Style', null);
+INSERT INTO idealfood.recipes (id, name, howto) VALUES (4, 'Light & Licious', null);
+INSERT INTO idealfood.recipes (id, name, howto) VALUES (5, 'Mother Earth', null);
+INSERT INTO idealfood.recipes (id, name, howto) VALUES (6, 'Cheesy', null);
