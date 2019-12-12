84fd429b5f95d37fb5ae8a93b8236f030818140f
Rasmus Jannerup <54896192+rasm211d@users.noreply.github.com>
2019-12-12 10:13:30 +0100

    skal åbenbart commit .iml og .xml

--- Implementering/src/GUI/Main.java
+++ Implementering/src/GUI/Main.java
@@ -4,6 +4,7 @@ package GUI;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,6 +15,8 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     private Scene startScene;
+    private Scene chooseIngredientsScene;
+    public Button findRecipiesButton;
     Stage stage = new Stage();
 
     @Override
@@ -21,9 +24,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Stage root = FXMLLoader.load(getClass().getResource("start.fxml"));
         root.show();
-//        primaryStage.setTitle("IdealFood");
-//        primaryStage.setScene(new Scene(root, 546, 822));
-//        primaryStage.show();
+//      primaryStage.setTitle("IdealFood");
+//      primaryStage.setScene(new Scene(root, 546, 822));
+//      primaryStage.show();
     }
 
 
@@ -31,17 +34,40 @@ public class Main extends Application {
         launch(args);
     }
 
-    public void popupChooseIngredients (javafx.event.ActionEvent actionEvent) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("chooseIngredients.fxml"));
-        Parent root1 = (Parent) fxmlLoader.load();
+    public void changeSceneToChooseIngredientsScene (javafx.event.ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("chooseIngredients.fxml"));
+        if(chooseIngredientsScene == null) {
+            chooseIngredientsScene = new Scene(root);
+        }
+
+        Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        primaryStage.setScene(chooseIngredientsScene);
+        primaryStage.show();
+    }
+
+    public void popupChooseIngredients(javafx.event.ActionEvent actionEvent) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("chooseIngredients.fxml"));
+        Stage root = fxmlLoader.load();
 
         stage.setTitle("Choose Ingredients");
-        stage.setScene(new Scene(root1));
-        stage.show();
+        root.show();
 
+    }
+
+    public void findRecipiesSaveButton() {
 
 
     }
+
+    public void closePopup() {
+        Stage stage = (Stage) findRecipiesButton.getScene().getWindow();
+        stage.close();
+    }
+
+
+
+
 
 
 
